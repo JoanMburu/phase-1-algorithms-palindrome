51db409d9f1dec42a6aa7568ea50ee4707fb2ac0
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-29 21:29:10 +0300

    updates algorithms palindrome

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  const cleanedWord = word.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  let start = 0;
+  let end = cleanedWord.length - 1;
+  
+  while (start < end) {
+    if (cleanedWord[start] !== cleanedWord[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  
+  return true;
 }
 
 module.exports = isPalindrome;
+
+// Test cases
+console.log("1) returns true for 'abba'");
+console.log("=>", isPalindrome("abba"));
+
+console.log("");
+
+console.log("2) returns true for 'racecar'");
+console.log("=>", isPalindrome("racecar"));
+
+console.log("");
+
+console.log("3) returns true for 'a'");
+console.log("=>", isPalindrome("a"));
+
+console.log("");
+
+console.log("4) returns false for 'robot'");
+console.log("=>", isPalindrome("robot"));
+
+console.log("");
+
+console.log("5) returns false for 'ab'");
+console.log("=>", isPalindrome("ab"));
